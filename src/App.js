b56b64b0c987ee533c7ebb4dc58176a1b0ebab05
pisutthee <pisut.flyinggiraffe@gmail.com>
2021-12-14 13:49:19 +0700

    complete filter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import AddTodo from './components/AddTodo';
+import RemainingMessage from './components/RemainingMessage';
+import SearchBar from './components/SearchBar';
+import TodoList from './components/TodoList';
+
+const initialTodoList = [
+  { id: uuidv4(), title: 'Watching a movie', completed: false },
+  { id: uuidv4(), title: 'Meeting a doctor', completed: false },
+  { id: uuidv4(), title: 'Dinner with my family', completed: true }
+];
+
+// const previousTodo = [a, b, c] // nextTodo = [d, a, b, c]
+// previousTodo.unshift(d)
+// const nextTodo = previousTodo.slice(0);
+// nextTodo.splice(0, 0, d)
+// const nextTodo = [d, ...previousTodo];
+// setTodoList(nextTodo);
 
 function App() {
+  const [todoList, setTodoList] = useState(initialTodoList);
+  const [searchText, setSearchText] = useState('abc');
+  const [searchStatus, setSearchStatus] = useState('');
+
+  const createTodo = title => {
+    const nextTodo = [
+      { id: uuidv4(), title: title, completed: false },
+      ...todoList
+    ];
+    setTodoList(nextTodo);
+  };
+
+  const deleteTodo = id => {
+    const idx = todoList.findIndex(item => item.id === id);
+    const newTodoList = [...todoList];
+    if (idx !== -1) {
+      newTodoList.splice(idx, 1);
+    }
+    setTodoList(newTodoList);
+  };
+
+  const updateTodo = (id, value) => {
+    const idx = todoList.findIndex(item => item.id === id);
+    const newTodoList = [...todoList];
+    if (idx !== -1) {
+      newTodoList[idx] = { ...newTodoList[idx], ...value };
+    }
+    setTodoList(newTodoList);
+  };
+
+  const pendingTodoList = todoList.filter(item => !item.completed);
+
+  const filteredTodoList = todoList.filter(
+    item =>
+      item.title.toLowerCase().includes(searchText.toLowerCase()) &&
+      (searchStatus === '' || item.completed === searchStatus)
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="mt-5 mx-auto mw-xs">
+        <AddTodo createTodo={createTodo} />
+        <SearchBar
+          searchStatus={searchStatus}
+          setSearchStatus={setSearchStatus}
+          setSearchText={setSearchText}
+          searchText={searchText}
+        />
+        <RemainingMessage
+          remaining={pendingTodoList.length}
+          total={todoList.length}
+        />
+        <TodoList
+          todoList={filteredTodoList}
+          deleteTodo={deleteTodo}
+          updateTodo={updateTodo}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/TextFilter.js
@@ -0,0 +1,23 @@
+function TextFilter(props) {
+  const handleChangeText = e => {
+    props.setSearchText(e.target.value);
+  };
+  return (
+    <div className="input-group">
+      <input
+        type="text"
+        className="form-control rounded-0"
+        value={props.searchText}
+        onChange={handleChangeText}
+      />
+      <button
+        className="btn btn-secondary rounded-0"
+        onClick={() => props.setSearchText('')}
+      >
+        <i className="fas fa-times" />
+      </button>
+    </div>
+  );
+}
+
+export default TextFilter;
